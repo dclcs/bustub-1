0561d8a94786bc985d03fd681aa8d07415a7d0a2
Duy Nguyen <duynguyen.ori75@gmail.com>
2020-06-09 17:48:20 +0700

    Finish hash table header page

--- src/storage/page/hash_table_header_page.cpp
+++ src/storage/page/hash_table_header_page.cpp
@@ -10,25 +10,49 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include <stdexcept>
+
 #include "storage/page/hash_table_header_page.h"
 
 namespace bustub {
-page_id_t HashTableHeaderPage::GetBlockPageId(size_t index) { return 0; }
+page_id_t HashTableHeaderPage::GetBlockPageId(size_t index) {
+	return this->block_page_ids_[index];
+}
 
-page_id_t HashTableHeaderPage::GetPageId() const { return 0; }
+page_id_t HashTableHeaderPage::GetPageId() const {
+	return this->page_id_;
+}
 
-void HashTableHeaderPage::SetPageId(bustub::page_id_t page_id) {}
+void HashTableHeaderPage::SetPageId(bustub::page_id_t page_id) {
+	this->page_id_ = page_id;
+}
 
-lsn_t HashTableHeaderPage::GetLSN() const { return 0; }
+lsn_t HashTableHeaderPage::GetLSN() const {
+	return this->lsn_;
+}
 
-void HashTableHeaderPage::SetLSN(lsn_t lsn) {}
+void HashTableHeaderPage::SetLSN(lsn_t lsn) {
+	this->lsn_ = lsn;
+}
 
-void HashTableHeaderPage::AddBlockPageId(page_id_t page_id) {}
+void HashTableHeaderPage::AddBlockPageId(page_id_t page_id) {
+	if (next_ind_ > this->GetSize()) {
+		throw std::invalid_argument("Reach limit of block_page_ids_");
+	}
+	this->block_page_ids_[next_ind_] = page_id;
+	next_ind_ ++;
+}
 
-size_t HashTableHeaderPage::NumBlocks() { return 0; }
+size_t HashTableHeaderPage::NumBlocks() {
+	return this->next_ind_;
+}
 
-void HashTableHeaderPage::SetSize(size_t size) {}
+void HashTableHeaderPage::SetSize(size_t size) {
+	this->size_ = size;
+}
 
-size_t HashTableHeaderPage::GetSize() const { return 0; }
+size_t HashTableHeaderPage::GetSize() const {
+	return this->size_;
+}
 
 }  // namespace bustub
