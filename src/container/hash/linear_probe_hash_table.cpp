7539bbb3e14e23ddadd6785d8c9749ea59a0ca39
Duy Nguyen <duynguyen.ori75@gmail.com>
2020-06-16 11:36:35 +0700

    Finish concurrency implementation - add more tests later

--- src/container/hash/linear_probe_hash_table.cpp
+++ src/container/hash/linear_probe_hash_table.cpp
@@ -41,7 +41,7 @@ HASH_TABLE_TYPE::LinearProbeHashTable(const std::string &name, BufferPoolManager
  *****************************************************************************/
 template <typename KeyType, typename ValueType, typename KeyComparator>
 bool HASH_TABLE_TYPE::GetValue(Transaction *transaction, const KeyType &key, std::vector<ValueType> *result) {
-  // TODO(duynl58) - Use RWLatch and transaction to lock the page
+  this->table_latch_.RLock();
   auto header_page = this->HeaderPage();
   auto expected_index = this->GetSlotIndex(key);
   auto meet_starting_point = false;
@@ -50,13 +50,26 @@ bool HASH_TABLE_TYPE::GetValue(Transaction *transaction, const KeyType &key, std
       if (meet_starting_point) break;
       meet_starting_point = true;
     }
-    auto block = this->BlockPage(header_page, index / BLOCK_ARRAY_SIZE);
+    // initialize block_page and block (casting)
+    auto block_index = index / BLOCK_ARRAY_SIZE;
+    auto page = this->buffer_pool_manager_->FetchPage(header_page->GetBlockPageId(block_index));
+    auto block = reinterpret_cast<HashTableBlockPage<KeyType, ValueType, KeyComparator> *>(page->GetData());
+
+    // expected offset of key-value pair in this block
     auto data_offset_in_block = index % BLOCK_ARRAY_SIZE;
-    if (!block->IsOccupied(data_offset_in_block)) break;
+
+    // start searching
+    page->RLatch();
+    if (!block->IsOccupied(data_offset_in_block)) {
+      page->RUnlatch();
+      break;
+    }
     if (block->IsReadable(data_offset_in_block) && this->comparator_(key, block->KeyAt(data_offset_in_block)) == 0) {
       result->push_back(block->ValueAt(data_offset_in_block));
     }
+    page->RUnlatch();
   }
+  this->table_latch_.RUnlock();
   if (result->size() > 0) return true;
   return false;
 }
@@ -66,7 +79,7 @@ bool HASH_TABLE_TYPE::GetValue(Transaction *transaction, const KeyType &key, std
  *****************************************************************************/
 template <typename KeyType, typename ValueType, typename KeyComparator>
 bool HASH_TABLE_TYPE::Insert(Transaction *transaction, const KeyType &key, const ValueType &value) {
-  // TODO(duynl58) - Lock this transaction, possibly lock the `block page` before inserting
+  this->table_latch_.RLock();
   auto result = std::vector<ValueType>();
   this->GetValue(transaction, key, &result);
   if (std::find(result.begin(), result.end(), value) != result.end()) {
@@ -80,16 +93,24 @@ bool HASH_TABLE_TYPE::Insert(Transaction *transaction, const KeyType &key, const
       if (meet_starting_point) break;
       meet_starting_point = true;
     }
-    auto block_id = index / BLOCK_ARRAY_SIZE;
-    auto block = this->BlockPage(header_page, block_id);
+    // initialize block_page and block (casting)
+    auto block_index = index / BLOCK_ARRAY_SIZE;
+    auto page = this->buffer_pool_manager_->FetchPage(header_page->GetBlockPageId(block_index));
+    auto block = reinterpret_cast<HashTableBlockPage<KeyType, ValueType, KeyComparator> *>(page->GetData());
+
+    // inserting (and flushing the page if the insert operator is successfuly)
+    page->WLatch();
     auto success = block->Insert(index % BLOCK_ARRAY_SIZE, key, value);
     if (success) {
-      this->buffer_pool_manager_->FlushPage(block_id);
+      this->buffer_pool_manager_->FlushPage(page->GetPageId());
+      page->WUnlatch();
+      this->table_latch_.RUnlock();
       return true;
     }
+    page->WUnlatch();
   }
+  this->table_latch_.RUnlock();
   // come here mean the current hash table is full, we need to resize
-  // TODO(duynl58) - Unlock all pages and transaction before resizing
   this->Resize(header_page->GetSize());
   return this->Insert(transaction, key, value);
 }
@@ -99,7 +120,7 @@ bool HASH_TABLE_TYPE::Insert(Transaction *transaction, const KeyType &key, const
  *****************************************************************************/
 template <typename KeyType, typename ValueType, typename KeyComparator>
 bool HASH_TABLE_TYPE::Remove(Transaction *transaction, const KeyType &key, const ValueType &value) {
-  // TODO(duynl58) - Lock this transaction, possibly lock the `block page` before removing
+  this->table_latch_.RLock();
   auto header_page = this->HeaderPage();
   auto expected_index = this->GetSlotIndex(key);
   auto meet_starting_point = false;
@@ -108,15 +129,30 @@ bool HASH_TABLE_TYPE::Remove(Transaction *transaction, const KeyType &key, const
       if (meet_starting_point) break;
       meet_starting_point = true;
     }
-    auto block = this->BlockPage(header_page, index / BLOCK_ARRAY_SIZE);
+    // initialize block_page and block (casting)
+    auto block_index = index / BLOCK_ARRAY_SIZE;
+    auto page = this->buffer_pool_manager_->FetchPage(header_page->GetBlockPageId(block_index));
+    auto block = reinterpret_cast<HashTableBlockPage<KeyType, ValueType, KeyComparator> *>(page->GetData());
+    // expected offset of key-value pair in this block
     auto data_offset_in_block = index % BLOCK_ARRAY_SIZE;
-    if (!block->IsOccupied(data_offset_in_block)) break;
+
+    // conditions
+    page->WLatch();
+    if (!block->IsOccupied(data_offset_in_block)) {
+      page->WUnlatch();
+      break;
+    }
     if (block->IsReadable(data_offset_in_block) && this->comparator_(key, block->KeyAt(data_offset_in_block)) == 0 &&
         value == block->ValueAt(data_offset_in_block)) {
+      // removing and unlock
       block->Remove(data_offset_in_block);
+      page->WUnlatch();
+      this->table_latch_.RUnlock();
       return true;
     }
+    page->WUnlatch();
   }
+  this->table_latch_.RUnlock();
   return false;
 }
 
@@ -125,29 +161,31 @@ bool HASH_TABLE_TYPE::Remove(Transaction *transaction, const KeyType &key, const
  *****************************************************************************/
 template <typename KeyType, typename ValueType, typename KeyComparator>
 void HASH_TABLE_TYPE::Resize(size_t initial_size) {
-  // TODO(duynl58) Lock this hash table before resizing
+  this->table_latch_.WLock();
   auto header_page = this->HeaderPage();
   auto expected_size = initial_size * 2;
   // only grow up in size
-  if (header_page->GetSize() > expected_size) return;
-  auto old_size = header_page->GetSize();
-  header_page->SetSize(expected_size);
-  this->appendBuckets(header_page, expected_size - old_size);
-  // re-organize all key-value pairs
-  auto all_old_blocks = std::vector<HashTableBlockPage<KeyType, ValueType, KeyComparator> *>();
-  auto all_block_page_ids = std::vector<page_id_t>();
-  for (size_t idx = 0; idx < header_page->NumBlocks(); idx++) {
-    all_old_blocks.push_back(this->BlockPage(header_page, idx));
-    all_block_page_ids.push_back(header_page->GetBlockPageId(idx));
-  }
-  header_page->ResetBlockIndex();
-  for (size_t idx = 0; idx < header_page->NumBlocks(); idx++) {
-    const auto &block = all_old_blocks[idx];
-    for (size_t pair_idx = 0; pair_idx < BLOCK_ARRAY_SIZE; pair_idx++) {
-      this->Insert(nullptr, block->KeyAt(pair_idx), block->ValueAt(pair_idx));
+  if (header_page->GetSize() < expected_size) {
+    auto old_size = header_page->GetSize();
+    header_page->SetSize(expected_size);
+    this->appendBuckets(header_page, expected_size - old_size);
+    // re-organize all key-value pairs
+    auto all_old_blocks = std::vector<HashTableBlockPage<KeyType, ValueType, KeyComparator> *>();
+    auto all_block_page_ids = std::vector<page_id_t>();
+    for (size_t idx = 0; idx < header_page->NumBlocks(); idx++) {
+      all_old_blocks.push_back(this->BlockPage(header_page, idx));
+      all_block_page_ids.push_back(header_page->GetBlockPageId(idx));
     }
-    this->buffer_pool_manager_->DeletePage(all_block_page_ids[idx]);
+    header_page->ResetBlockIndex();
+    for (size_t idx = 0; idx < header_page->NumBlocks(); idx++) {
+      const auto &block = all_old_blocks[idx];
+      for (size_t pair_idx = 0; pair_idx < BLOCK_ARRAY_SIZE; pair_idx++) {
+        this->Insert(nullptr, block->KeyAt(pair_idx), block->ValueAt(pair_idx));
+      }
+      this->buffer_pool_manager_->DeletePage(all_block_page_ids[idx]);
+    }
   }
+  this->table_latch_.WUnlock();
 }
 
 /*****************************************************************************
@@ -155,12 +193,14 @@ void HASH_TABLE_TYPE::Resize(size_t initial_size) {
  *****************************************************************************/
 template <typename KeyType, typename ValueType, typename KeyComparator>
 size_t HASH_TABLE_TYPE::GetSize() {
-  // TODO(duynl58) Acquire READ latch before return Size()
-  return this->HeaderPage()->GetSize();
+  this->table_latch_.RLock();
+  auto size = this->HeaderPage()->GetSize();
+  this->table_latch_.RUnlock();
+  return size;
 }
 
 /*****************************************************************************
- * UTILITIES
+ * UTILITIES (these functions should already be called in a lock context)
  *****************************************************************************/
 template <typename KeyType, typename ValueType, typename KeyComparator>
 HashTableHeaderPage *HASH_TABLE_TYPE::HeaderPage() {
